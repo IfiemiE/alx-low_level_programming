67d91ca16f26feef4d922f978ebdc087dc62283b
ifiemiE <erigiifiemi@gmail.com>
2022-09-08 12:42:31 +0000

    first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
-# include <stdio.h>
-/*A program to display a string */
+#include <stdio.h>
+/**
+ * main - display a string with the function puts
+ * Return:0 to end.
+ */
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle");
