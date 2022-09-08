3cbcee3daf8033e090a8ee4397e31e419eade813
ifiemiE <erigiifiemi@gmail.com>
2022-09-08 15:12:53 +0000

    modify code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <inttypes.h>
 /**
  * main - displays the sizes in bytes of some data types
  * Return: 0 to terminate program
@@ -10,10 +11,10 @@ int size_int;
 long int size_L;
 long long int size_LL;
 float size_f;
-printf("Size of a char: %c byte(s)\n", sizeof(size_char));
+printf("Size of a char: %d byte(s)\n", sizeof(size_char));
 printf("Size of an int: %d byte(s)\n", sizeof(size_int));
-printf("Size of a long int: %ld byte(s)\n", sizeof(size_L));
-printf("Size of a long long int: %lld byte(s)\n", sizeof(size_LL));
-printf("Size of a float: %f byte(s)\n", sizeof(size_f));
+printf("Size of a long int: %d byte(s)\n", sizeof(size_L));
+printf("Size of a long long int: %d byte(s)\n", sizeof(size_LL));
+printf("Size of a float: %d byte(s)\n", sizeof(size_f));
 return (0);
 }
