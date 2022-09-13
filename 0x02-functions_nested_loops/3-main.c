1f9af5b9cd5ca25740842b241bd6714b87a99563
ifiemiE <erigiifiemi@gmail.com>
2022-09-13 21:32:24 +0000

    lower case check

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - test run the _islower code
+ * Return: 0 always
+ */
+int main(void)
+{
+	int r;
+
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
