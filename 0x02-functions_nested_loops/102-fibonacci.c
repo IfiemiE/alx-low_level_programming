8bc43d8cd2f94be215e381c2ecc7db8a593313c1
ifiemiE <erigiifiemi@gmail.com>
2022-09-14 13:33:14 +0000

    first 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ * main - displays Fibonacci numbers
+ *
+ * Return: 0 for successful run
+ */
+int main(void)
+{
+	int f1 = 1;
+	int f2 = 2;
+	long int F;
+	int i;
+
+	for (i = 1; i <= 50; i++)
+	{
+		if (i == 1)
+		{
+			F = f1;
+		}
+		else if (i == 2)
+		{
+			F = f2;
+		}
+		else if (i == 3)
+		{
+			F = f1 + f2;
+		}
+		else
+		{
+			f1 = f2;
+			f2 = F;
+			F = f1 + f2;
+		}
+
+		if (i == 1)
+		{
+			printf("%lu", F);
+		}
+		else
+		{
+			printf(", %lu", F);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
