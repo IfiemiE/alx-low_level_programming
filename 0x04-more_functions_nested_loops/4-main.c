4a6c9a1220c546c533bf6e4f5431291c78179866
ifiemiE <erigiifiemi@gmail.com>
2022-09-15 21:37:31 +0000

    all digits except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* main - to check code of print_most_function
+* Return: 0 always
+*/
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,5 +7,6 @@ int _isupper(char);
 int _isdigit(char);
 int mul(int, int);
 void print_numbers(void);
+void print_most_numbers(void);
 
 #endif
