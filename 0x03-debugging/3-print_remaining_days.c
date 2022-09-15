8815779a884f94553420b89f2b34f5be92f38121
ifiemiE <erigiifiemi@gmail.com>
2022-09-15 09:45:20 +0000

    counting the remaining days of the year

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month
+* @year: year
+* Return: void
+*/
+
+void print_remaining_days(int month, int day, int year)
+{
+	int days = convert_day(month, day);
+
+
+
+	if ((((year % 4) == 0) && ((year % 100) != 0)) || ((year % 400) == 0))
+	{
+/*
+*		printf("leap year\n");
+*/
+		if (((month == 2) && (day > 29)))
+		{
+			printf("Invalid date\n");
+		}
+		else
+		{
+			if (month > 2)
+			{
+				days++;
+			}
+			printf("Day of the year: %d\n", days);
+			printf("Remaining days: %d\n", 366 - days);
+		}
+
+	}
+	else
+	{
+		if ((month == 2) && (day > 28))
+		{
+			printf("Invalid date\n");
+		}
+		else
+		{
+/*
+*			printf("Not leap\n");
+*/
+			printf("Day of the year: %d\n", days);
+			printf("Remaining days: %d\n", 365 - days);
+		}
+	}
+}
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,2 +1,4 @@
 0: debugging an output  with input 0.
 1: commenting out an infinite loop
+2: displaying the largest of 3 numbers
+3: printing the remaining days of a year, given a date in the year
