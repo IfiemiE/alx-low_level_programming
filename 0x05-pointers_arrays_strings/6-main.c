1d925d519cc12e3ecb4dc7f84c37b38a8de5b461
ifiemiE <erigiifiemi@gmail.com>
2022-09-19 11:57:39 +0000

    print even character positions

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - tests code
+* Return: 0 always
+*/
+int main(void)
+{
+char *str;
+
+str = "0123456789";
+puts2(str);
+return (0);
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* puts2 - prints all except 2nd and last
+*@str: given string
+*/
+void puts2(char *str)
+{
+	int count = 0;
+	int k, i;
+
+	for (i = 0; *(str + i); i++)
+	{
+		count++;
+	}
+	for (k = 0; k <= count; k += 2)
+	{
+		_putchar(*(str + k));
+	}
+	_putchar('\n');
+}
