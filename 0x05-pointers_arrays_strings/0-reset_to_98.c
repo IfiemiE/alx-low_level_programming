f45cd17ab7efbc536b6b60342a13df4c646836f9
ifiemiE <erigiifiemi@gmail.com>
2022-09-19 08:59:00 +0000

    my first pointer project

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* reset_to_98 - resets input argument to 98
+*@n: input argument
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+void _putchar(char);
+void reset_to_98(int *n);
+
+
+
+
+
+#endif
