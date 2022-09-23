f6e99abb2cb2584627b8b8993e32f6e103b3f4dc
ifiemiE <erigiifiemi@gmail.com>
2022-09-23 08:21:36 +0000

    hot fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,20 +6,19 @@
 */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char *alpha = "aAeEoOtTlL";
+	char *l33t = "4433007711";
 
 	for (i = 0; *(s + i); i++)
 	{
-		if ((*(s + i) == 'a') || (*(s + i) == 'A'))
-			*(s + i) = '4';
-		else if ((*(s + i) == 'e') || (*(s + i) == 'E'))
-			*(s + i) = '3';
-		else if ((*(s + i) == 'o') || (*(s + i) == 'O'))
-			*(s + i) = '0';
-		else if ((*(s + i) == 't') || (*(s + i) == 'T'))
-			*(s + i) = '7';
-		else if ((*(s + i) == 'l') || (*(s + i) == 'L'))
-			*(s + i) = '1';
+		for (j = 0; j < 10; j++)
+		{
+			if (*(s + i) == *(alpha + j))
+			{
+				*(s + i) = *(l33t + j);
+			}
+		}
 	}
 	return (s);
 }
