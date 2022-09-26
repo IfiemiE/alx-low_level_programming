2e80d34175fa2ff8d88be7ac8b70043a54f3577a
ifiemiE <erigiifiemi@gmail.com>
2022-09-26 15:31:34 +0000

    rescreating strchr

---
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - tests code
+* Return: 0 always
+*/
+int main(void)
+{
+
+return (0);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 void _putchar(char);
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
-
+char *_strchr(char *, char);
 
 
 
