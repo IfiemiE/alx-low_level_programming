9427ca28106f2c82033aef49290d8fcf16751a64
ifiemiE <erigiifiemi@gmail.com>
2022-09-27 00:54:36 +0000

    hot fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,38 +9,20 @@
   */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, k;
-	char *start = NULL;
-	int len_needle = 0, len_hay = 0;
+	char *h;
+	char *n;
 
-	for (i = 0; *(needle + i); i++)
-		len_needle++;
-
-	for (i = 0; *(haystack + i); i++)
-		len_hay++;
-
-	if ((len_needle != 0) && (len_hay != 0))
+	for (; *haystack != '\0'; haystack++)
 	{
-		for (i = 0; *(haystack + i) != '\0'; i++)
+		h = haystack;
+		n = needle;
+		while ((*h == *n) && (*n != '\0'))
 		{
-			if (*(haystack + i) == *needle)
-			{
-				start = haystack + i;
-				j = i;
-				for (k = 0; *(needle + k) != '\0'; k++)
-				{
-					if (*(haystack + j) == *(needle + k))
-						j++;
-					else
-					{
-						start = NULL;
-						break;
-					}
-				}
-				if (*(needle + k) == '\0')
-					return (start);
-			}
+			h++;
+			n++;
 		}
+		if (*n == '\0')
+			return (haystack);
 	}
 	return (0);
 }
---
+++ 0x07-pointers_arrays_strings/strstr5.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+  * _strstr - locates first occurrence of a substring
+  * in a string
+  * @haystack: the given string
+  * @needle: the given substring
+  * Return: NULL if not found, or the pointer to the beginning
+  * of the located substring
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k;
+	char *start = NULL;
+	int len_needle = 0, len_hay = 0;
+
+	for (i = 0; *(needle + i); i++)
+		len_needle++;
+
+	for (i = 0; *(haystack + i); i++)
+		len_hay++;
+
+	if ((len_needle != 0) && (len_hay != 0))
+	{
+		for (i = 0; *(haystack + i) != '\0'; i++)
+		{
+			if (*(haystack + i) == *needle)
+			{
+				start = haystack + i;
+				j = i;
+				for (k = 0; *(needle + k) != '\0'; k++)
+				{
+					if (*(haystack + j) == *(needle + k))
+						j++;
+					else
+					{
+						start = NULL;
+						break;
+					}
+				}
+				if (*(needle + k) == '\0')
+					return (start);
+			}
+		}
+	}
+	return (0);
+}
