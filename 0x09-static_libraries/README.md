a9e7a886b557dbecbf79bcba61e33dc6940cd705
ifiemiE <erigiifiemi@gmail.com>
2022-09-30 20:21:58 +0000

    my static library

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,5 @@
+# create a static library (archive)
+
+## tasks:
+0: create a static library of some listed functions
+1: 
---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+int _putchar(char);
+int _isupper(char);
+char *_memset(char *, char, unsigned int);
+char *_strcat(char *, char *);
+int _isdigit(char);
+char *_memcpy(char *, char *, unsigned int);
+char *_strncat(char *, char *, int);
+int _atoi(char *);
+char *_strchr(char *, char);
+int _strlen(char *);
+char *_strncpy(char *, char *, int);
+int _islower(char);
+void _puts(char *str);
+int _strcmp(char *, char *);
+unsigned int _strspn(char *, char *);
+int _isalpha(int);
+char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
+int _abs(int);
+char *_strcpy(char *, char *);
+#endif
