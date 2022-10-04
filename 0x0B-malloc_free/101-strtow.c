88513f6527d74e95add0051a5d18a25f2679e4bf
ifiemiE <erigiifiemi@gmail.com>
2022-10-04 11:55:34 +0000

    debugged 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,20 @@
 #include "main.h"
 #include <stdlib.h>
+
+/**
+  * strLen - compute length of string
+  * @s: the given string
+  * Return: the length
+  */
+int strLen(char *s)
+{
+	int i, len = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+			len++;
+	return (len);
+}
+
 /**
   * countw - count number of words
   * in a string
@@ -14,13 +29,8 @@ int countw(char *s)
 	{
 		if (*(s + i) != ' ')
 		{
-			if (i == 0)
+			if ((i == 0) || (*(s + i - 1) == ' '))
 				w++;
-			else
-			{
-				if (*(s + i - 1) == ' ')
-					w++;
-			}
 		}
 	}
 	return (w);
@@ -56,37 +66,43 @@ int countchar(char *s)
 char **strtow(char *str)
 {
 	int i, j;
-	int c, wc = 0;
+	int c, wc, w = 0;
 	char **arr;
 
-	if ((str == NULL) || (*str == ' '))
+	if ((str == NULL) || (strLen(str) == 0))
 		return (NULL);
 	wc = countw(str);
-	arr = malloc(wc * sizeof(*arr));
+	arr = malloc((wc + 1) * sizeof(*arr));
 	if (arr == NULL)
 		return (NULL);
-
-	for (i = 0; i < wc; i++)
+	arr[wc] = NULL;
+	i = 0;
+	while (*(str + i) != '\0')
 	{
 		if (*(str + i) != ' ')
 		{
 			if ((i == 0) || (*(str + i - 1) == ' '))
 			{
 				c = countchar(str + i);
-				arr[i] = malloc((c + 1) * sizeof(*arr[i]));
-				if (arr[i] == NULL)
+				w++;
+				arr[w - 1] = malloc((c + 1) * sizeof(*arr[w - 1]));
+				if (arr[w - 1] == NULL)
 				{
-					for (j = i; j >= 0; j--)
-						free(arr[i]);
+					for (j = w - 1; j >= 0; j--)
+						free(arr[j]);
 					free(arr);
 					return (NULL);
 				}
 				for (j = 0; j < c; j++)
-					arr[i][j] = *(str + i + j);
-				arr[i][j] = '\0';
+					arr[w - 1][j] = *(str + i + j);
+				arr[w - 1][j] = '\0';
+				i += c;
 			}
+			else
+				i++;
 		}
+		else
+			i++;
 	}
-	arr[i] = NULL;
 	return (arr);
 }
