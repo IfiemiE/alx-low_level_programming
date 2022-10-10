79973dc4db101551bd6f0cd62b1678ac878b0346
ifiemiE <erigiifiemi@gmail.com>
2022-10-10 12:38:33 +0000

    print name of program file via macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - prints name of file from which
+  * program was compiled
+  * Return: 0 always
+  */
+int main(void)
+{
+	printf(__FILE__);
+	printf("\n");
+	return (0);
+}
