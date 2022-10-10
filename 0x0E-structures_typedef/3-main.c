98b2fdd228e8a8d65171d8d7a879b085765d6513
ifiemiE <erigiifiemi@gmail.com>
2022-10-10 21:03:18 +0000

    creating a new type

---
+++ 0x0E-structures_typedef/3-main.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+  * main - tests codes
+  * Return: 0 for success exit
+  */
+int main(void)
+{
+	dog_t my_dog;
+
+	my_dog.name = "poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+
+	printf("name: %s, age: %.1f, Owner: %s\n", my_dog.name,
+		my_dog.age, my_dog.owner);
+
+
+
+	return (0);
+}
