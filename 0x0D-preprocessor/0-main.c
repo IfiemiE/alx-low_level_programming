7adb5be91d022d07dab63923544ec73e81192915
ifiemiE <erigiifiemi@gmail.com>
2022-10-10 12:25:11 +0000

    MACRO in a header

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,14 @@
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+  * main - tests codes
+  * Return: 0 always
+  */
+int main(void)
+{
+	printf("%d", SIZE);
+	printf("\n");
+	return (0);
+}
+
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,4 @@
+## 0x0D - Preprocessor project
+
+# Tasks
+0: a header with an object_like macro
