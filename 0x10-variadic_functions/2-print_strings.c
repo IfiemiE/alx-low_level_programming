7227c3063a550ebc951e8a4915b066fb9a4a9b8b
ifiemiE <erigiifiemi@gmail.com>
2022-10-14 11:55:07 +0000

    print stream of string arguments

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 1; i <= n; i++)
 	{
 		str = va_arg(ptr, char *);
-		printf("%s", str);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 		if ((i < n) && (separator != NULL))
 			printf("%s", separator);
 	}
