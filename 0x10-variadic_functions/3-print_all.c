224f097ac01a0d5ddd298cdd8ddb8cf54686f301
ifiemiE <erigiifiemi@gmail.com>
2022-10-14 14:03:06 +0000

    formatted string-print

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,7 +14,7 @@ void print_all(const char * const format, ...)
 
 	va_start(ptr, format);
 	i = 0;
-	while (*(format + i) != '\0')
+	while ((format != NULL) && (*(format + i) != '\0'))
 	{
 		ch = *(format + i);
 		skipped = 0;
