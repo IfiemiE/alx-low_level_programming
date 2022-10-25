979e7d8b6b59c02ee81e91bcee2cd1296f898dbb
IfiemiE <erigiifiemi@gmail.com>
2022-10-25 14:30:52 +0000

    task 9 refix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -66,6 +66,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			*head = new_node;
 			new_node->next = NULL;
+			new_node->n = n;
 			return (new_node);
 		}
 		return (NULL);
