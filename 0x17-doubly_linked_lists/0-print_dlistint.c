8774747eb1b3efc6762224b601ce364839c53cb7
IfiemiE <erigiifiemi@gmail.com>
2022-12-08 19:39:08 +0000

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -20,8 +20,8 @@ size_t print_dlistint(const dlistint_t *h)
         {
 		printf("%d\n", h->n);
                 h = h->next;
-                counter++;
+                count++;
         }
 
-        return (counter);
+        return (count);
 }
